c14b4c792606e06bee5a6bc43094879924a7f756
xandaodev680 <alexandre424augusto@gmail.com>
2026-02-18 21:27:55 -0300

    Update app.run to use environment port and disable debug
    Change app to run on specified port and disable debug mode.

--- app.py
+++ app.py
@@ -304,4 +304,5 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)  # debug=False em produção
